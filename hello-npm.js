17d7b721c7409a1ed5dcb660696c0333adfcb565
alekseyrw <alekseyrw@gmail.com>
2017-06-19 14:26:06 +0300

    das

--- hello-npm.js
+++ hello-npm.js
@@ -1,20 +1,20 @@
 var ansi = require('ansi')
   , cursor = ansi(process.stdout)
  
-var s = 'Hello world!';
+var s = 'Hello world! :)';
 
 cursor.write('\n\n\n')
 
 s.split('').forEach(function(i){
 		
-		setTimeout(function(){
+		// setTimeout(function(){
 
 		cursor
 			.hex('#' + Math.random() * 1000000)
 			.write(i)
 			
-			} 
-			, 1000)
+			// } 
+			// , 1000)
 
 })
 
