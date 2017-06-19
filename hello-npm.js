f9ec9d7693b767fc35292e86f62b007a6e970769
alekseyrw <alekseyrw@gmail.com>
2017-06-19 13:48:58 +0300

    First commit

---
+++ hello-npm.js
@@ -0,0 +1,22 @@
+var ansi = require('ansi')
+  , cursor = ansi(process.stdout)
+ 
+var s = 'Hello world!';
+
+cursor.write('\n\n\n')
+
+s.split('').forEach(function(i){
+		
+		setTimeout(function(){
+
+		cursor
+			.hex('#' + Math.random() * 1000000)
+			.write(i)
+			
+			} 
+			, 1000)
+
+})
+
+
+cursor.write('\n\n\n')
